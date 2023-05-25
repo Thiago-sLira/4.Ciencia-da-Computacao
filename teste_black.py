b0eecd7990039838bb66734960b00a8ce21ac99e
Thiago Lira <thiago.slira@outlook.com>
2023-05-25 12:04:31 -0300

    Primeiro commit

---
+++ teste_black.py
@@ -0,0 +1,10 @@
+thiago = "pessoa estudante de python"
+
+
+dicas_pythonicas_sao_legais = True
+
+
+if dicas_pythonicas_sao_legais:
+    print("Dicas pythonicas são legais")
+else:
+    print("Dicas pythonicas não são legais")
