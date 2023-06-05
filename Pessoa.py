cf911fe0e1321c7ae00691e0a043ee9fa0035305
Thiago Lira <thiago.slira@outlook.com>
2023-06-05 15:30:40 -0300

    Criando classe Pessoa

--- Pessoa.py
+++ Pessoa.py
@@ -1,3 +1,6 @@
+import Liquidificador
+
+
 class Pessoa:
     def __init__(self, nome, saldo_na_conta):
         self.nome = nome
@@ -8,3 +11,8 @@ class Pessoa:
         if liquidificador.preco <= self.saldo_na_conta:
             self.saldo_na_conta -= liquidificador.preco
             self.liquidificador = liquidificador
+
+
+liquidificador_vermelho = Liquidificador("Rosa", "110", "127", "200")
+pessoa_cozinheira = Pessoa("Jacquin", 1000)
+pessoa_cozinheira.comprar_liquidificador(liquidificador_vermelho)
