c1f228df108a23b1de07d8026f87fd1de65f56b0
Thiago Lira <thiago.slira@outlook.com>
2023-06-05 16:13:50 -0300

    Criando uma classe mãe

--- Liquidificador.py
+++ Liquidificador.py
@@ -1,3 +1,5 @@
+from Eletrodomestico import Eletrodomestico
+
 # class Liquidificador:
 #     def get_cor(self):
 #         return self.__cor.upper()
@@ -60,31 +62,35 @@
 # # Após pintarmos, a nova cor é PRETO
 
 
-class Liquidificador:
-    # Getter
-    @property
-    def cor(self):
-        return self.__cor.upper()
+# class Liquidificador:
+#     # Getter
+#     @property
+#     def cor(self):
+#         return self.__cor.upper()
 
-    @cor.setter
-    def cor(self, nova_cor):
-        if nova_cor.lower() == "turquesa":
-            raise ValueError("Não existe liquidificador turquesa")
-        self.__cor = nova_cor
+#     @cor.setter
+#     def cor(self, nova_cor):
+#         if nova_cor.lower() == "turquesa":
+#             raise ValueError("Não existe liquidificador turquesa")
+#         self.__cor = nova_cor
 
-    def __init__(self, cor, potencia, tensao, preco):
-        self.cor = cor
-        self.potencia = potencia
-        self.tensao = tensao
-        self.preco = preco
+#     def __init__(self, cor, potencia, tensao, preco):
+#         self.cor = cor
+#         self.potencia = potencia
+#         self.tensao = tensao
+#         self.preco = preco
 
-    def __str__(self):
-        return f"liquidificador {self.cor} de {self.potencia}W"
+#     def __str__(self):
+#         return f"liquidificador {self.cor} de {self.potencia}W"
 
 
-liquidificador = Liquidificador("Rosa", "110", "127", "200")
+# liquidificador = Liquidificador("Rosa", "110", "127", "200")
 
 # print(liquidificador.cor)  # ROSA
 # liquidificador.cor = "Vermelho"
 # print(liquidificador.cor)  # VERMELHO
 # print(liquidificador.preco)
+
+
+class Liquidificador(Eletrodomestico):
+    pass
